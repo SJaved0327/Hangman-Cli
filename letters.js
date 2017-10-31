b1f6fe01dbaf646afe434c789294ae3e36a3bc54
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2017-10-30 22:45:17 -0400

    pseudocoding

--- letters.js
+++ letters.js
@@ -1,19 +1,30 @@
 
 //==============================//
 
+//individual letter from selected word passes into constructor
+//individual letter is stored as object
+//Letter
 var Letter = function(actualLetter){
-
-	this.currentDisplay = "_"
+	//letter prints to display as a blank(underscore)
+	this.currentDisplay = "_";
+	//value of the letter is stored as actualLetter
 	this.actualLetter = actualLetter;
+	//value of letter being guessed correctly starts as false
+	//value will switch to true when user guesses correctly
 	this.guessCorrect = false;
-
 };
 
+//function to check if the letter guessed is in the word
 Letter.prototype.letterInWord = function(guessLetter){
+	//if the value of the letter is equal to the value of letter being guessed
 	if(this.actualLetter == guessLetter){
+			//value of the letter prints to the display
 			this.currentDisplay = this.actualLetter; 
+			//value of letter being guessed correctly switches to true
 			this.guessCorrect = true;
 		}else{
+			//if they do not equal to each other, 
+			//value of letter being guessed correctly stays as false
 			this.guessCorrect = false;
 		};
 };
--- words.js
+++ words.js
@@ -1,19 +1,28 @@
+
 var Letter = require("./letters.js");
 
 //==============================//
 
-
-var Word = function (actualWord){
-	
+//word selected during the new game passes into constructor
+//individual word is stored as object
+//Word
+var Word = function(actualWord){
+	//value of the word is stored as actualWord
 	this.actualWord = actualWord;
+	//empty array will hold all the individual letters
 	this.wordArray = [];
+	//call function to populate array
 	this.RenderLetters();
-	
 };
 
+//splits word into individual letters and push each to array
+//individual letters passed into Letter constructor
+//RenderLetters
 Word.prototype.RenderLetters = function(actualWord){
 	for(var i = 0; i < this.actualWord.length; i++){
+		//passes each letter in actualWord through Letter constructor
 		var pushLetter = new Letter(this.actualWord[i]);
+		//pushes each Letter object into array
 		this.wordArray.push(pushLetter);
 	};
 };
